3cffdab9935088e87176c5a3e3fd49c00351c755
Siddhesh <gaonkarsiddhesh0907@gmail.com>
2023-12-24 19:12:03 +0530

    patterns

--- Patterns.java
+++ Patterns.java
@@ -66,7 +66,7 @@ public class Patterns {
     // public static void main(String[] args) {
     //     for(int i=1;i<=5;i++){
     //         for(int j=1;j<=i;j++){
-    //             System.out.print(j);
+    //             System.out.print(j + " ");
     //         }
     //         System.out.println();
     //     }
@@ -74,12 +74,41 @@ public class Patterns {
 
 
 
-    public static void main(String[] args) {
-        
-    }
+    // public static void main(String[] args) {
+    //     for(int i=5;i>=1;i--){
+    //         for(int j=1;j<=i;j++){
+    //             System.out.print(j + " ");
+    //         }
+    //         System.out.println();
+    //     }
+    // }
 
 
 
+    // public static void main(String[] args) {
+    //     int num = 1;
+    //     int n=5;
+    //     for(int i=1;i<=n;i++){
+    //         for(int j=1; j<=i;j++){
+    //             System.out.print((num++) + " ");
+    //         }
+    //         System.out.println();
+    //     }
+    // }
 
+    
 
+    // public static void main(String[] args) {
+    //     for(int i=1; i<=5; i++){
+    //         for(int j=1; j<=i; j++){
+    //             if ((i+j)%2==0) {
+    //                 System.out.print("1 " );
+    //             }
+    //             else{
+    //                 System.out.print("0 ");
+    //             }
+    //         }
+    //         System.out.println();
+    //     }
+    // }
 }
